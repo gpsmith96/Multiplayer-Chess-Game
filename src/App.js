73e10728489016c40264049fc85c196dc34f70c3
gpsmith96 <48976136+gpsmith96@users.noreply.github.com>
2023-03-04 17:28:33 -0500

    removed port from websocket URL

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     super();
     this.state = {
         GameIsActive : false,
-        socket : io("https://chess-game-server.onrender.com:8000", {
+        socket : io("https://chess-game-server.onrender.com", {
           transports : ['websocket', 'polling'],
           withCredentials : true
         }),
