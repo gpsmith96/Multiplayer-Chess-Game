dcd91ade433e1dfe71e50645433891e7f82d8910
gpsmith96 <48976136+gpsmith96@users.noreply.github.com>
2023-03-19 11:33:59 -0400

    Add rules for check and checkmate, minor edits

--- src/Game.js
+++ src/Game.js
@@ -11,13 +11,12 @@ class Game extends React.Component {
     for (let index = 0; index < 64; index++) {
       let coordX = (index % 8) + 1;
       let coordY = Math.floor(index / 8) + 1
-      let piece = {};
       let tile = {
         id : uuidv4(),
         coordX : coordX,
         coordY : coordY,
         piece : {},
-        color : (((Math.floor(index / 8) % 2) + index) % 2) == 1 ? '#c3c3ea' : "White",
+        color : (((Math.floor(index / 8) % 2) + index) % 2) === 1 ? '#c3c3ea' : "White",
         selected : ""
       };
       tempBoard.push(tile);
@@ -33,7 +32,13 @@ class Game extends React.Component {
         vectors : StartingPieces[i].vectors
       }
     }
-    this.state = {board : tempBoard, selectedPiece : {}, user : "", moveList : [], activePlayer : "White"}
+    this.state = {board : tempBoard, 
+    selectedPiece : {}, 
+    user : "", 
+    moveList : [], 
+    activePlayer : "White",
+    check : {"White" : false, "Black" : false},
+    };
     this.handleClickTile = this.handleClickTile.bind(this);
     this.handleForfeit = this.handleForfeit.bind(this);
   }
@@ -42,19 +47,23 @@ class Game extends React.Component {
 
     this.props.socket.on("send_move_list", (data, activePlayer) => {
       let workingBoard = this.state.board.map(tile=>(tile));
+      let workingMoveList = this.state.moveList.map(move=>(move));
       data.forEach((move, index) => {
-        if (JSON.stringify(move) !== JSON.stringify(this.state.moveList[index])){
+        if (JSON.stringify(move) !== JSON.stringify(workingMoveList[index])){
 
-          // const prevTile = workingBoard.filter(tile => (tile.coordX === move.prevX && tile.coordY === move.prevY))[0];
-          // const nextTile = workingBoard.filter(tile => (tile.coordX === move.nextX && tile.coordY === move.nextY))[0];
-
-          this.MovePieces (move, workingBoard, move.player);
+          this.MovePieces(move, workingBoard);
+          workingMoveList.push(move);
         }
       });
-
+      const checkmate = this.isCheckmate(workingBoard)
+      if(checkmate[this.state.user]){
+        this.props.socket.emit("forfeit");
+      }
       this.setState({
         board : workingBoard, 
-        activePlayer : activePlayer
+        check : this.checkThreaten(workingBoard),
+        activePlayer : activePlayer,
+        moveList : workingMoveList
       });
     });
   }
@@ -83,10 +92,14 @@ class Game extends React.Component {
         tile.selected = "";
         return tile;});
     }
-    this.setState({board : workingBoard});
+
+    this.setState({
+      board : workingBoard,
+      check : this.checkThreaten(workingBoard)
+    });
   }
 
-  MovePieces (move, workingBoard, player) {
+  MovePieces (move, workingBoard) {
     const prevTile = workingBoard.filter(tile => (tile.coordX === move.prevX && tile.coordY === move.prevY))[0];
     const nextTile = workingBoard.filter(tile => (tile.coordX === move.nextX && tile.coordY === move.nextY))[0];
     workingBoard = workingBoard.map(tile=>{
@@ -99,15 +112,53 @@ class Game extends React.Component {
     workingBoard = workingBoard.map(tile=>{
       if (tile.id === prevTile.id) tile.piece = {};
       return tile;});
-    move.player = player;
-    this.setState((prevState) => {
-      prevState.moveList.push(move);
-      return {
-        moveList : prevState.moveList,
-        selectedPiece : {}
+    return workingBoard;
+  }
+
+  checkThreaten(workingBoard) {
+    var playersInCheck = {"White" : false, "Black" : false};
+    workingBoard.forEach((tile) => {
+      if(!isEmpty(tile.piece)) {
+        let possibleMoves = [];
+        let startingpoint = {X : tile.coordX, Y : tile.coordY};
+        possibleMoves = possibleMoves.concat(this.calculateMoves(workingBoard, tile.piece.vectors, startingpoint, tile.piece.firstMove, tile.piece.owner));
+        possibleMoves.forEach((move) => {
+          if (!isEmpty(move.piece) && move.piece.type === "King" && move.piece.owner !== tile.piece.owner) {
+            playersInCheck[move.piece.owner] = true;
+          }
+        });
       }
     });
-    return workingBoard;
+    return playersInCheck;
+  }
+
+  isCheckmate(workingBoard){
+    let checkmate = {White : false, Black : false};
+    for (let player in checkmate) {
+      let possibleMoves = [];
+      workingBoard.forEach((tile) => {
+        if(!isEmpty(tile.piece) && tile.piece.owner === player){
+          let startingpoint = {X : tile.coordX, Y : tile.coordY};
+          possibleMoves = possibleMoves.concat(
+            this.calculateMoves(workingBoard, tile.piece.vectors, startingpoint, tile.piece.firstMove, player));
+          possibleMoves = possibleMoves.filter((endTile) => {
+            const move = {
+              prevX : tile.coordX,
+              prevY : tile.coordY,
+              nextX : endTile.coordX,
+              nextY : endTile.coordY,
+              piece : tile.piece.type,
+              player : tile.piece.owner
+            };
+            var duplicateBoard = JSON.parse(JSON.stringify(workingBoard));
+            const checkAfterMove = this.checkThreaten(this.MovePieces(move, duplicateBoard));
+            return !checkAfterMove[player];
+          });
+        }
+      });
+      checkmate[player] = (possibleMoves.length === 0);
+    }
+    return checkmate;
   }
 
   highlightPiece (selectedTile, workingBoard) {
@@ -115,15 +166,28 @@ class Game extends React.Component {
       tile.selected = "";
       return tile;});
 
-    if (selectedTile.piece == {} || selectedTile.selected !== "") selectedTile.selected = ""
+    if (selectedTile.piece === {} || selectedTile.selected !== "") selectedTile.selected = ""
     else if (selectedTile.piece.owner === this.state.user) selectedTile.selected = "lightBlue" //TODO check user id against piece owner
     else selectedTile.selected = ""
 
     let possibleMoves = [];
     if (selectedTile.selected !== ""){ //Determine possible moves
-      let vectors = [];
       let startingpoint = {X : selectedTile.coordX, Y : selectedTile.coordY};
-      possibleMoves = possibleMoves.concat(this.calculateMoves(workingBoard, selectedTile.piece.vectors, startingpoint, selectedTile.piece.firstMove));
+      possibleMoves = possibleMoves.concat(
+        this.calculateMoves(workingBoard, selectedTile.piece.vectors, startingpoint, selectedTile.piece.firstMove, this.state.user));
+      possibleMoves = possibleMoves.filter((endTile) => {
+        const move = {
+          prevX : selectedTile.coordX,
+          prevY : selectedTile.coordY,
+          nextX : endTile.coordX,
+          nextY : endTile.coordY,
+          piece : selectedTile.piece.type,
+          player : this.state.user
+        };
+        var duplicateBoard = JSON.parse(JSON.stringify(workingBoard));
+        const checkAfterMove = this.checkThreaten(this.MovePieces(move, duplicateBoard));
+        return !checkAfterMove[this.state.user];
+      });
     }
 
     for (let option in possibleMoves) {
@@ -133,7 +197,7 @@ class Game extends React.Component {
   return workingBoard ;
   }
 
-  calculateMoves(board, vectors, coords, firstMove) {
+  calculateMoves(board, vectors, coords, firstMove, player) {
     let possibleMoves = [];
     for (let vector in vectors) {
       let testingCoords = {X : coords.X, Y : coords.Y};
@@ -143,7 +207,7 @@ class Game extends React.Component {
       testingCoords.Y += vectors[vector].Y;
       while (dist < maxDist && testingCoords.X < 9 && testingCoords.Y < 9 && testingCoords.X > 0 && testingCoords.Y > 0) {
         if (!isEmpty(board[(testingCoords.X - 1) + (testingCoords.Y - 1) * 8].piece)) {
-          if (board[(testingCoords.X - 1) + (testingCoords.Y - 1) * 8].piece.owner !== this.state.user && vectors[vector].canAttack !== "no")
+          if (board[(testingCoords.X - 1) + (testingCoords.Y - 1) * 8].piece.owner !== player && vectors[vector].canAttack !== "no")
             possibleMoves.push(board[(testingCoords.X - 1) + (testingCoords.Y - 1) * 8]);
           break;
         }
@@ -167,6 +231,8 @@ class Game extends React.Component {
           ? "Your turn"
           : "Waiting for opponent"
         }
+        {(this.state.check[this.state.user]) ? "You are in Check!" : ""}
+        {(this.state.check[(this.state.user === "White") ? "Black" : "White"]) ? "Opponent in Check!" : ""}
         <Board user={this.state.user} handleClick={this.handleClickTile} tiles={this.state.board}/>
         <button onClick={this.handleForfeit}>Forfeit</button>
         </div>
