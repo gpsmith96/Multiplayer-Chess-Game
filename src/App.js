fb038cb0970091a6d19e7f3a32afc449732a3983
gpsmith96 <48976136+gpsmith96@users.noreply.github.com>
2023-03-24 00:23:30 -0400

    chatbox update
    moved some functionality to be handled by the app to allow the chatbox to be persistent during the game

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Game from './Game';
 import StartPage from './StartPage';
 import EndGame from './EndGame';
+import { v4 as uuidv4 } from 'uuid';
+
 const io = require("socket.io-client");
 
 class App extends React.Component {
@@ -17,15 +19,23 @@ class App extends React.Component {
         }),
         color: "White",
         gameOver : false,
-        winner : ""
+        winner : "",
+        roomID : "",
+        message : "",
+        chatLog : "",
+        username : "Player",
       }
     this.startGame = this.startGame.bind(this);
     this.endGame = this.endGame.bind(this);
+    this.resetGame = this.resetGame.bind(this);
+    this.setRoomID = this.setRoomID.bind(this);
+    this.setUsername = this.setUsername.bind(this);
+    this.handleSend = this.handleSend.bind(this);
   }
 
   componentDidMount() {
     this.state.socket.on("connect", () => {
-      this.state.socket.send(this.state.socket.id + " connected");
+      this.state.socket.emit("registerUsername", this.state.username);
       this.forceUpdate();
     });
     this.state.socket.on("disconnect", () => {
@@ -33,9 +43,19 @@ class App extends React.Component {
       this.forceUpdate();
     });
 
-    this.state.socket.on("message", (source, data) => {
-      console.log(source + ": " + data);
+    this.state.socket.on("chat_message", (username, data) => {
+      console.log(data);
+      this.setState((prevState) => ({chatLog : prevState.chatLog + "\n" + username + ":    " + data}));
     });
+
+    this.state.socket.on("start_game", (role) => {
+      console.log("Start game as " + role);
+      this.startGame(role);
+    });
+
+    // this.state.socket.on("message", (source, data) => {
+    //   console.log(source + ": " + data);
+    // });
 
     this.state.socket.on("end_game", (winner) => {
       console.log("End Game");
@@ -51,22 +71,52 @@ class App extends React.Component {
     console.log("Game over, the winner is " + winner)
     this.setState({
       gameOver : true,
-      winner : winner
+      winner : winner,
+      GameIsActive : false
     });
+  }
+
+  resetGame() {
+    this.setState({
+      gameOver : false,
+      winner : "",
+      GameIsActive : false
+    });
+  }
+
+  setRoomID(value) {
+    this.setState({roomID : value});
+  }
+
+  setUsername(value) {
+    this.state.socket.emit("registerUsername", value);
+    this.setState({username : value});
+  }
+  handleSend() {
+    this.state.socket.emit("broadcast", this.state.roomID, this.state.message);
+    this.setState((prevState) => ({message : ""}));
   }
 
   render() {
     return (
       <div className="App">
-        {
+        <div className="chatBox">
+          <ul>{this.state.chatLog.split("\n").map((text) => (<li key={uuidv4()}>{text}</li>))}</ul>
+          <input value={this.state.message} onChange={(e) => {this.setState({message : e.target.value})}}/>
+          <button onClick={this.handleSend}>Send</button>
+        </div>
+        <div className="gameContainer">{
         !this.state.socket.connected 
           ? "connecting"
           : this.state.gameOver
-            ? <EndGame winner={this.state.winner}/>
+            ? <EndGame winner={this.state.winner} resetGame={this.resetGame} color={this.state.color}/>
             : this.state.GameIsActive 
               ? <Game socket={this.state.socket} color={this.state.color}/>
-              : <StartPage startGame={this.startGame} socket={this.state.socket}/>
-        }
+              : <StartPage socket={this.state.socket} 
+              setRoomID={this.setRoomID} roomID={this.state.roomID}
+              setUsername={this.setUsername} username={this.state.username}
+              chatLog={this.state.chatLog}/>
+        }</div>
       </div>
     );
   }
--- src/StartPage.js
+++ src/StartPage.js
@@ -1,47 +1,30 @@
 import React from 'react';
 import './StartPage.css';
-import { v4 as uuidv4 } from 'uuid';
 
 class StartPage extends React.Component {
   constructor() {
     super();
     this.state = {
-        roomID : "",
         inputRoomID : "",
         message : "",
         inputUsername : "",
-        username : "Player",
-        chatLog : "",
         playerCount : 0
       }
     this.handleJoin = this.handleJoin.bind(this);
-    this.handleSend = this.handleSend.bind(this);
     this.handleUser = this.handleUser.bind(this);
     this.handleStart = this.handleStart.bind(this);
   }
 
 
   componentDidMount() {
-    this.props.socket.on("connect_ack", () => {
-      this.props.socket.emit("registerUsername", this.state.username);
-    });
-
-    this.props.socket.on("message", (username, data) => {
-      console.log(data);
-      this.setState((prevState) => ({chatLog : prevState.chatLog + "\n" + username + ":    " + data}));
-    });
-
     this.props.socket.on("ack", (room, num) => {
-      this.setState((prevState) => ({roomID : room, playerCount : num}));
-    });
-
-    this.props.socket.on("start_game", (role) => {
-      console.log("Start game as " + role);
-      this.props.startGame(role);
+      this.props.setRoomID(room);
+      this.setState({playerCount : num});
     });
 
     this.props.socket.on("join_success", () => {
-      this.setState((prevState) => ({roomID : prevState.inputRoomID, inputRoomID : ""}));
+      this.props.setRoomID(this.state.inputRoomID);
+      this.setState({inputRoomID : ""});
     });
 
     this.props.socket.on("join_fail", () => {
@@ -52,15 +35,10 @@ class StartPage extends React.Component {
     console.log("requesting to join room " + this.state.inputRoomID);
     this.props.socket.emit("join room", this.state.inputRoomID);
   }
-  handleSend() {
-    this.props.socket.emit("broadcast", this.state.roomID, this.state.message);
-    // this.setState((prevState) => ({message : "", chatLog : prevState.chatLog + "\n" + this.state.username + ":    " + this.state.message}));
-    this.setState((prevState) => ({message : ""}));
-  }
 
   handleUser() {
-    this.props.socket.emit("registerUsername", this.state.inputUsername);
-    this.setState((prevState) => ({username : prevState.inputUsername, inputUsername : ""}));
+    this.props.setUsername(this.state.inputUsername);
+    this.setState({inputUsername : ""});
   }
 
   handleStart() {
@@ -71,18 +49,15 @@ class StartPage extends React.Component {
   render(){
     return (
       <div className="StartPage">
-        <h1>Chess game   {this.state.playerCount}/2</h1>
-        Username: <input value={this.state.inputUsername} onChange={(e) => {this.setState({inputUsername : e.target.value})}}/>
+        <div className="Heading">Chess game</div>
+        <div className="Heading">Current room: {this.props.roomID} </div>
+        <div>Username: {this.props.username}</div> 
+        <div>Players in room: {this.state.playerCount}/2</div>
+        Change Username: <input value={this.state.inputUsername} onChange={(e) => {this.setState({inputUsername : e.target.value})}}/>
         <button onClick={this.handleUser}>Set Username</button>
-        <div>Username: {this.state.username} Current room: {this.state.roomID}</div>
-        Room to join: <input value={this.state.inputRoomID} onChange={(e) => {this.setState({inputRoomID : e.target.value})}}/>
+        Join a Room: <input value={this.state.inputRoomID} onChange={(e) => {this.setState({inputRoomID : e.target.value})}}/>
         <button onClick={this.handleJoin}>Join</button>
-        Message to send: <input value={this.state.message} onChange={(e) => {this.setState({message : e.target.value})}}/>
-        <button onClick={this.handleSend}>Send</button>
         {(this.state.playerCount == 2) ? <button className="StartButton" onClick={this.handleStart}>Start Game</button>:<></>}
-        <div className="chatBox">
-          <ul>{this.state.chatLog.split("\n").map((text) => (<li key={uuidv4()}>{text}</li>))}</ul>
-        </div>
       </div>
     );
   }
